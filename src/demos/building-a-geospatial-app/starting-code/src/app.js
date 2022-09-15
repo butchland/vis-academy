a682ec1376d033c8471025a60626c446571b0e80
Butch Landingin <butchland@gmail.com>
2022-09-15 21:21:31 +0800

    feat: Replace MapGL with  DeckGL and StaticMap

--- src/demos/building-a-geospatial-app/starting-code/src/app.js
+++ src/demos/building-a-geospatial-app/starting-code/src/app.js
@@ -1,64 +1,78 @@
 import React, { Component } from 'react';
-import MapGL from 'react-map-gl';
-import { MapStylePicker } from './controls';
+// import MapGL from 'react-map-gl';
+import { StaticMap } from 'react-map-gl';
+// import { MapStylePicker } from './controls';
+import DeckGL from 'deck.gl';
 
-const MAPBOX_STYLE = 'mapbox://styles/mapbox/dark-v9';
-const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+// const MAPBOX_STYLE = 'mapbox://styles/mapbox/dark-v9';
+// const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
 
+const INITIAL_VIEW_STATE = {
+  longitude: -74,
+  latitude: 40.7,
+  zoom: 11,
+  minZoom: 5,
+  maxZoom: 22,
+  pitch: 0,
+  bearing: 0
+}
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      style: MAPBOX_STYLE,
-      viewport: {
-        width: window.innerWidth,
-        height: window.innerHeight,
-        longitude: -74,
-        latitude: 40.7,
-        zoom: 11,
-        maxZoom: 22
-      }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     style: MAPBOX_STYLE,
+  //     viewport: {
+  //       width: window.innerWidth,
+  //       height: window.innerHeight,
+  //       longitude: -74,
+  //       latitude: 40.7,
+  //       zoom: 11,
+  //       maxZoom: 22
+  //     }
 
-    };
-  }
-  onStyleChange = (style) => {
-    this.setState({ style });
-  }
+  //   };
+  // }
+  // onStyleChange = (style) => {
+  //   this.setState({ style });
+  // }
 
-  _onViewportChange(viewport) {
-    this.setState({
-      viewport: { ...this.state.viewport, ...viewport }
-    });
-  }
+  // _onViewportChange(viewport) {
+  //   this.setState({
+  //     viewport: { ...this.state.viewport, ...viewport }
+  //   });
+  // }
 
-  componentDidMount() {
-    window.addEventListener('resize', this._resize);
-    this._resize();
-  }
+  // componentDidMount() {
+  //   window.addEventListener('resize', this._resize);
+  //   this._resize();
+  // }
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this._resize);
-  }
+  // componentWillUnmount() {
+  //   window.removeEventListener('resize', this._resize);
+  // }
 
-  _resize = () => {
-    this._onViewportChange({
-      width: window.innerWidth,
-      height: window.innerHeight
-    });
-  }
+  // _resize = () => {
+  //   this._onViewportChange({
+  //     width: window.innerWidth,
+  //     height: window.innerHeight
+  //   });
+  // }
 
   render() {
     return (
       <div>
-        <MapStylePicker onStyleChange={this.onStyleChange} currentStyle={this.state.style} />
+        <DeckGL initialViewState={INITIAL_VIEW_STATE} controller>
+          <StaticMap />
+        </DeckGL>
+        {/* <MapStylePicker onStyleChange={this.onStyleChange} currentStyle={this.state.style} />
         <MapGL
           {... this.state.viewport}
           mapStyle={this.state.style}
           mapboxApiAccessToken={MAPBOX_TOKEN}
           onViewportChange={viewport => this._onViewportChange(viewport)}
         >
-        </MapGL>
+        </MapGL> */}
       </div>
     );
   }
