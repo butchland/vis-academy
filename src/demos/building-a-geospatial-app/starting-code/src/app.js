61e8e521b9659eb44dcff50d87fcae7fd397a5ed
Butch Landingin <butchland@gmail.com>
2022-09-15 22:09:19 +0800

    feat: Add scatterplot layer to map

--- src/demos/building-a-geospatial-app/starting-code/src/app.js
+++ src/demos/building-a-geospatial-app/starting-code/src/app.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { StaticMap } from 'react-map-gl';
 import { MapStylePicker } from './controls';
 import DeckGL from 'deck.gl';
+import taxiData from '../../../data/taxi';
+import { renderLayers } from './deckgl-layers';
 
 const MAPBOX_STYLE = 'mapbox://styles/mapbox/dark-v9';
 const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
@@ -18,17 +20,48 @@ const INITIAL_VIEW_STATE = {
 
 export default class App extends Component {
   state = {
+    points: [],
     style: MAPBOX_STYLE
   };
   
   onStyleChange = (style) => {
     this.setState({ style });
   }
- 
+
+  componentDidMount() {
+    this._processData();
+    // ...
+  }
+
+  _processData() {
+    const points = taxiData.reduce((accu, curr) => {
+      accu.push({
+        position: [Number(curr.pickup_longitude), Number(curr.pickup_latitude)],
+        pickup: true
+      });
+      accu.push({
+        position: [
+          Number(curr.dropoff_longitude),
+          Number(curr.dropoff_latitude)
+        ],
+        pickup: false
+      });
+      return accu;
+    }, []);
+    this.setState({
+      points
+    });
+  }
+
+
   render() {
     return (
       <div>
-        <DeckGL initialViewState={INITIAL_VIEW_STATE} controller>
+        <DeckGL
+          layers={renderLayers({data: this.state.points})} 
+          initialViewState={INITIAL_VIEW_STATE} 
+          controller
+        >
           <MapStylePicker 
             onStyleChange={this.onStyleChange} 
             currentStyle={this.state.style} 
