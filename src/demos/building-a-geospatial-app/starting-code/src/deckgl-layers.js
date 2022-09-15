ae11cbeefe5feb682f309228313b24ef88c0bf57
Butch Landingin <butchland@gmail.com>
2022-09-15 22:08:29 +0800

    feat: Add a scatterplot layer that renders plight blue points

--- src/demos/building-a-geospatial-app/starting-code/src/deckgl-layers.js
+++ src/demos/building-a-geospatial-app/starting-code/src/deckgl-layers.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import { ScatterplotLayer } from 'deck.gl';
 
 const PICKUP_COLOR = [114, 19, 108];
 const DROPOFF_COLOR = [243, 185, 72];
 
 export function renderLayers(props) {
-  return [];
+  const { data } = props;
+  return [
+    new ScatterplotLayer({
+      points: [],
+      id: 'scatterplot',
+      getPosition: d => d.position,
+      getColor: d => [0, 128, 255],
+      getRadius: d => 5,
+      opacity: 0.5,
+      pickable: true,
+      points: [], 
+      radiusMinPixels: 0.25,
+      radiusMaxPixels: 30,
+      data
+    })
+  ];
 }
