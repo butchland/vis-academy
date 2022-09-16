6858fb8d5bb8338ada685b19caa116c2ec18f91f
Butch Landingin <butchland@gmail.com>
2022-09-16 13:29:59 +0000

    chore: Add constants for hexagon layer

--- src/demos/building-a-geospatial-app/starting-code/src/deckgl-layers.js
+++ src/demos/building-a-geospatial-app/starting-code/src/deckgl-layers.js
@@ -3,6 +3,27 @@ import { ScatterplotLayer } from 'deck.gl';
 
 const PICKUP_COLOR = [114, 19, 108];
 const DROPOFF_COLOR = [243, 185, 72];
+// in RGB
+
+const HEATMAP_COLORS = [
+  [255, 255, 204],
+  [199, 233, 180],
+  [127, 205, 187],
+  [65, 182, 196],
+  [44, 127, 184],
+  [37, 52, 148]
+];
+
+const LIGHT_SETTINGS = {
+  lightsPosition: [-73.8, 40.5, 8000, -74.2, 40.9, 8000],
+  ambientRatio: 0.4,
+  diffuseRatio: 0.6,
+  specularRatio: 0.2,
+  lightsStrength: [0.8, 0.0, 0.8, 0.0],
+  numberOfLights: 2
+};
+
+const elevationRange = [0, 1000];
 
 export function renderLayers(props) {
   const { data, onHover, settings } = props;
